896eb880de65dd01f45cc013098de839aeeedc3a
Trixy Parra <trixy.parra@gmail.com>
2023-05-04 02:51:57 -0700

    changed form to div so it won't submit - refressh page - when button clicks

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,11 +2,19 @@
 export default function Meme() {
     return (
         <main>
-            <form className="form">
-                <input type="text" placeholder="Top text" className="top-text-input" />
-                <input type="text" placeholder="Bottom text" className="bottom-text-input" />
+            <div className="form">
+                <input 
+                    type="text" 
+                    placeholder="Top text" 
+                    className="top-text-input" 
+                />
+                <input 
+                    type="text" 
+                    placeholder="Bottom text" 
+                    className="bottom-text-input" 
+                />
                 <button className="form-btn">Get a New Meme Image ✨</button>
-            </form>
+            </div>
         </main>
     )
 }
