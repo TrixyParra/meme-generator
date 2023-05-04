2ad5af6d4b26ffbaf08c219053f594f9f90bb2c4
Trixy Parra <trixy.parra@gmail.com>
2023-05-04 01:03:04 -0700

    created components folder - started on Header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import TrollFaceLogo from '../images/troll-face-logo.png';
+
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img src={TrollFaceLogo} className="logo-image" />
+            <h3>Meme Generator</h3>
+        </header>
+    )
+}
