775491d3223ebd23c022b09bf627e0c649bb4ffb
Trixy Parra <trixy.parra@gmail.com>
2023-05-04 01:35:05 -0700

    added alt to the image

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import TrollFaceLogo from '../images/troll-face-logo.png';
 export default function Header() {
     return (
         <header className="header">
-            <img src={TrollFaceLogo} className="logo-image" />
+            <img src={TrollFaceLogo} alt='Troll Face Logo' className="logo-image" />
             <h3>Meme Generator</h3>
         </header>
     )
